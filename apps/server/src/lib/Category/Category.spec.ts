7f40a39acfee99b96e648dacc68b77b038b23380
Carlos Frias <carlos.a.frias@gmail.com>
2020-05-24 19:26:01 -0300

    Add Category E2E test

--- apps/server/src/lib/Category/Category.spec.ts
+++ apps/server/src/lib/Category/Category.spec.ts
@@ -1,5 +1,48 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import DatabaseConnection from '../DB/DatabaseConnection';
+import CategoryService from './CategoryService';
+import CategoryModel from './Category';
+
 describe('CategoryModel', () => {
+  const connection = new DatabaseConnection();
+  const mongoServer = new MongoMemoryServer();
+
+  beforeAll(async () => {
+    const uri = await mongoServer.getUri();
+    return connection.connect(uri);
+  });
+
+  afterAll(async () => {
+    await connection.disconnect();
+    await mongoServer.stop();
+  });
+
   describe('post save', () => {
-    xit('saves parent', () => {});
+    it('saves parent', async () => {
+      const category1 = await CategoryService.createCategory({
+        icon: 'http://icon.com/',
+        name: 'Cat 1',
+      });
+      const category2 = await CategoryService.createCategory({
+        icon: 'http://icon.com',
+        name: 'Cat 2',
+        parent: category1,
+      });
+
+      await category2.save();
+
+      const dbCat1 = await CategoryModel.findOne({ name: 'Cat 1' }).populate(
+        'children'
+      );
+      expect(dbCat1).not.toBeNull();
+      expect(dbCat1?.children[0].name).toBe('Cat 2');
+
+      const dbCat2 = await CategoryModel.findOne({ name: 'Cat 2' }).populate(
+        'parent'
+      );
+      expect(dbCat2).not.toBeNull();
+      expect(dbCat2?.parent).toBeDefined();
+      expect(dbCat2?.parent?.name).toBe('Cat 1');
+    });
   });
 });
--- apps/server/src/lib/DB/DatabaseConnection.ts
+++ apps/server/src/lib/DB/DatabaseConnection.ts
@@ -5,10 +5,13 @@ import { singleton } from 'tsyringe';
 export default class DatabaseConnection {
   connection?: typeof import('mongoose');
 
-  async connect(): Promise<void> {
-    const url = process.env.DB_URL;
+  async connect(uri?: string): Promise<void> {
+    let url = uri;
     if (!url) {
-      throw new Error('DB_URL environment variable must be set');
+      url = process.env.DB_URL;
+      if (!url) {
+        throw new Error('DB_URL environment variable must be set');
+      }
     }
 
     try {
@@ -28,5 +31,10 @@ export default class DatabaseConnection {
       throw new Error('Connection has not been established');
     }
     return this.connection;
+  }
+
+  disconnect(): Promise<void> {
+    const connection = this.getConnection();
+    return connection.disconnect();
   }
 }
