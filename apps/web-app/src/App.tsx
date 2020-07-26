812a49b53849b9b9ffa0fd6eba96c133647e9640
Carlos Frias <carlos.a.frias@gmail.com>
2020-07-26 14:25:52 -0300

    Add boilerplate

---
+++ apps/web-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+import React from 'react';
+
+const App: React.FunctionComponent = () => (
+  <main>
+    <h1 className="bg-blue-500 mt-5">Yap!</h1>
+  </main>
+);
+
+export default App;
---
+++ apps/web-app/webpack.common.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.tsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.ejs',
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
